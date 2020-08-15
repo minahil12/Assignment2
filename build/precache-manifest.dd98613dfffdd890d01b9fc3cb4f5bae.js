e4d14dff04552656240038894375ae385d65eae0
minahil12 <60703733+minahil12@users.noreply.github.com>
2020-08-15 18:47:46 +0500

    Add files via upload

---
+++ build/precache-manifest.dd98613dfffdd890d01b9fc3cb4f5bae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b3e30b162068bd5cdda9576e5db874f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b67e6c46e41c47d45e4",
+    "url": "/static/css/2.a09d231f.chunk.css"
+  },
+  {
+    "revision": "392a87588f62b9a76d3c",
+    "url": "/static/css/main.f6b1c804.chunk.css"
+  },
+  {
+    "revision": "2b67e6c46e41c47d45e4",
+    "url": "/static/js/2.3ce2b82b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.3ce2b82b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "392a87588f62b9a76d3c",
+    "url": "/static/js/main.9c9381c1.chunk.js"
+  },
+  {
+    "revision": "0cf7ba42e8e472256b87",
+    "url": "/static/js/runtime-main.63e340d2.js"
+  }
+]);
